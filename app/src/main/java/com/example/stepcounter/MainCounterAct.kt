6d68d9da4c7927b30fd0f1c679aeac75590c0d91
HiepPD <hieppdutck59@gmail.com>
2023-03-13 14:53:34 +0700

    first commit

---
+++ app/src/main/java/com/example/stepcounter/MainCounterAct.kt
@@ -0,0 +1,11 @@
+package com.example.stepcounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainCounterAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_main_counter)
+    }
+}
